b7593a81ccfe595132b4eeb8ae12949e9280ce54
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-10 20:24:48 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,14 +1,14 @@
 def turn_count(board)
- counter = 0
- if counter % 2 == 0
-  return #something
-elsif counter % 2 == 1
-  return #something
-else
+
 end
 
-def current_player
-
+def current_player(index)
+  counter = 0
+  if counter % 2 == 0
+   return index = "X"
+ elsif counter % 2 == 1
+   return index = "O"
+ else
 end
 
 #if counter
